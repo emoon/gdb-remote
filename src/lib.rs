0c63d2a05f37fdee9ecc44a2c1e6e0c49dfe48a1
Daniel Collin <daniel@collin.com>
2016-05-02 18:26:55 +0200

    Some work on the lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,41 @@
 use std::net::TcpStream;
 use std::net::SocketAddr;
-use std::str::FromStr;
-use std::str;
 use std::io::Write;
 use std::io;
 
+pub enum NeedsAck {
+    Yes,
+    No,
+}
+
 pub struct GdbRemote {
-    pub stream: TcpStream,
-    pub needs_ack: bool,
+    pub stream: Option<TcpStream>,
+    pub needs_ack: NeedsAck,
 }
 
 static HEX_CHARS: &'static [u8; 16] = b"0123456789abcdef";
 
 impl GdbRemote {
-    pub fn connect(addr: &SocketAddr) -> Option<GdbRemote> {
-        match TcpStream::connect(addr).map(|stream| stream) {
-            Ok(s) => Some(GdbRemote { stream: s, needs_ack: true }),
-            Err(_) => None,
+    pub fn new(needs_ack: NeedsAck) -> GdbRemote {
+        GdbRemote {
+            stream: None,
+            needs_ack: needs_ack,
         }
     }
 
-    pub fn calc_checksum(data: &[u8]) -> u8 {
+    pub fn new_and_connect(addr: &SocketAddr, needs_ack: NeedsAck) -> io::Result<GdbRemote> {
+        match TcpStream::connect(addr).map(|stream| stream) {
+            Ok(s) => Ok(GdbRemote { stream: Some(s), needs_ack: needs_ack }),
+            Err(e) => Err(e),
+        }
+    }
+
+    pub fn connect(&mut self, addr: &SocketAddr) -> io::Result<()> {
+        self.stream = Some(try!(TcpStream::connect(addr)));
+        Ok(())
+    }
+
+    fn calc_checksum(data: &[u8]) -> u8 {
         let mut checksum = 0u32;
         for i in data {
             checksum += *i as u32;
@@ -33,19 +48,6 @@ impl GdbRemote {
         let cs_string: [u8; 2] = [HEX_CHARS[((checksum >> 4) & 0xf) as usize],
                                   HEX_CHARS[(checksum & 0xf) as usize]];
         cs_string
-    }
-
-    pub fn calc_checksum_array(data: &[u8]) -> [u8; 2] {
-        let checksum = Self::calc_checksum(data);
-        Self::get_checksum_array(checksum)
-    }
-
-    pub fn get_checksum_string(checksum: u8) -> String {
-        let cs_string = Self::get_checksum_array(checksum);
-        unsafe {
-            // Use unchecked here because we always know the range we use
-            String::from_str(str::from_utf8_unchecked(&cs_string)).unwrap()
-        }
     }
 
     pub fn get_processed_string(data: &str) -> String {
@@ -64,11 +66,23 @@ impl GdbRemote {
         gdb_string
     }
 
+    pub fn send_command(&mut self, data: &str) -> io::Result<usize> {
+        let command = Self::get_processed_string(data);
+        if let Some(ref mut stream) = self.stream {
+            stream.write(command.as_str().as_bytes())
+        } else {
+            Ok((0))
+        }
+    }
+
     pub fn send_simple_command(&mut self, command: char) -> io::Result<usize> {
-        // # command $ checksum
         let cs = Self::get_checksum_array(command as u8);
         let data: [u8; 5] = [b'$', command as u8, b'#', cs[0], cs[1]];
-        self.stream.write(&data)
+        if let Some(ref mut stream) = self.stream {
+            stream.write(&data)
+        } else {
+            Ok((0))
+        }
     }
 }
 
@@ -76,15 +90,6 @@ impl GdbRemote {
 fn test_checksum_calc() {
     let data: [u8; 8] = [32, 32, 32, 32, 64, 64, 64, 64];
     assert_eq!(GdbRemote::calc_checksum(&data), 128);
-}
-
-#[test]
-fn test_create_checksum_string() {
-    assert_eq!(GdbRemote::get_checksum_string(0xf0), "f0");
-    assert_eq!(GdbRemote::get_checksum_string(0xff), "ff");
-    assert_eq!(GdbRemote::get_checksum_string(0x2f), "2f");
-    assert_eq!(GdbRemote::get_checksum_string(0x87), "87");
-    assert_eq!(GdbRemote::get_checksum_string(0xba), "ba");
 }
 
 #[test]
